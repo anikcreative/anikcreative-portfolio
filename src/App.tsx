0d13b06026948fe85d1bd9292b7e0e44843bb923
Anik Bhattacharya <anik.bhattacharya@hcss.com>
2019-11-18 00:22:47 -0600

    some cleanup and removal of unneeded files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="app-container">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
